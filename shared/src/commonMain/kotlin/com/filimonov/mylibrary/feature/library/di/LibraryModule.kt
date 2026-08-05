c345565cb262b051b74901758b0169999baf05c6
Andrey Filimonov <andrfil05@mail.ru>
2026-08-05 11:54:25 +0600

    feat(library): create LibraryModule

---
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/di/LibraryModule.kt
@@ -0,0 +1,43 @@
+package com.filimonov.mylibrary.feature.library.di
+
+import com.filimonov.mylibrary.feature.library.data.repository.BookRepositoryImpl
+import com.filimonov.mylibrary.feature.library.domain.repository.BookRepository
+import com.filimonov.mylibrary.feature.library.domain.usecase.AddBookUseCase
+import com.filimonov.mylibrary.feature.library.domain.usecase.DeleteBookUseCase
+import com.filimonov.mylibrary.feature.library.domain.usecase.GetBooksUseCase
+import com.filimonov.mylibrary.feature.library.domain.usecase.UpdateBookUseCase
+import com.filimonov.mylibrary.feature.library.presentation.LibraryViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val libraryModule = module {
+
+    single<BookRepository> {
+        BookRepositoryImpl(get())
+    }
+
+    factory {
+        GetBooksUseCase(get())
+    }
+
+    factory {
+        AddBookUseCase(get())
+    }
+
+    factory {
+        DeleteBookUseCase(get())
+    }
+
+    factory {
+        UpdateBookUseCase(get())
+    }
+
+    viewModel {
+        LibraryViewModel(
+            get(),
+            get(),
+            get(),
+            get()
+        )
+    }
+}
