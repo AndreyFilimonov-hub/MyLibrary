d06bb1bfba0ef9cc328c7e156d43090cfbf69223
Andrey Filimonov <andrfil05@mail.ru>
2026-08-05 11:33:00 +0600

    feat(library): create BookRepositoryImpl and mapper

---
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/data/mapper/BookMapper.kt
@@ -0,0 +1,25 @@
+package com.filimonov.mylibrary.feature.library.data.mapper
+
+import com.filimonov.mylibrary.core.database.entity.BookDbModel
+import com.filimonov.mylibrary.feature.library.domain.model.Book
+
+
+fun BookDbModel.toDomain() = Book(
+    id = id,
+    title = title,
+    author = author,
+    path = path,
+    coverPath = coverPath,
+    isFavorite = isFavorite,
+    isRead = isRead
+)
+
+fun Book.toDbModel() = BookDbModel(
+    id = id,
+    title = title,
+    author = author,
+    path = path,
+    coverPath = coverPath,
+    isFavorite = isFavorite,
+    isRead = isRead
+)
