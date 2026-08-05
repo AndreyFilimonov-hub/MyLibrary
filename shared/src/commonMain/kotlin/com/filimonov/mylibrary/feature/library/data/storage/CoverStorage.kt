a648fde0cd84efa25fd51464a224303d7d42a109
Andrey Filimonov <andrfil05@mail.ru>
2026-08-05 17:08:08 +0600

    feat(library): create EpubParser and CoverStorage

---
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/data/storage/CoverStorage.kt
@@ -0,0 +1,32 @@
+package com.filimonov.mylibrary.feature.library.data.storage
+
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.PlatformFile
+import io.github.vinceglb.filekit.filesDir
+import io.github.vinceglb.filekit.path
+import io.github.vinceglb.filekit.write
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import okio.SYSTEM
+
+class CoverStorage {
+
+    init {
+        val coversDir = "${FileKit.filesDir.path}/covers".toPath()
+        FileSystem.SYSTEM.createDirectories(coversDir)
+    }
+
+    suspend fun saveCover(
+        bytes: ByteArray,
+        fileName: String
+    ): String {
+        val file = PlatformFile(
+            FileKit.filesDir,
+            "covers/$fileName.jpg"
+        )
+
+        file.write(bytes)
+
+        return file.path
+    }
+}
