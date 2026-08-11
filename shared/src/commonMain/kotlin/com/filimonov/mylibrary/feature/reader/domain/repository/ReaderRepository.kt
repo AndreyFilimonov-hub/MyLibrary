8ce0668f327be6a06d668e47ab2822e5b4f5265a
Andrey Filimonov <andrfil05@mail.ru>
2026-08-11 16:41:45 +0600

    feat(reader): add usecases

--- shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/repository/ReaderRepository.kt
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/repository/ReaderRepository.kt
@@ -1,8 +1,14 @@
 package com.filimonov.mylibrary.feature.reader.domain.repository
 
 import com.filimonov.mylibrary.feature.reader.domain.model.Chapter
+import com.filimonov.mylibrary.feature.reader.domain.model.ReaderSettings
+import kotlinx.coroutines.flow.Flow
 
 interface ReaderRepository {
 
     suspend fun getBookById(bookId: Long): List<Chapter>
+
+    fun getReaderSettings(): Flow<ReaderSettings>
+
+    suspend fun saveReaderSettings(settings: ReaderSettings)
 }
