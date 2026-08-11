4c3e1f0d3f6f55a11a2ff967733c76bcf6d0a1df
Andrey Filimonov <andrfil05@mail.ru>
2026-08-11 16:40:23 +0600

    feat(reader): create domain models

--- shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/model/ReaderSettings.kt
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/model/ReaderSettings.kt
@@ -1,9 +1,9 @@
 package com.filimonov.mylibrary.feature.reader.domain.model
 
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
-
 data class ReaderSettings (
-    val fontSize: TextUnit = 18.sp,
-    val lineHeight: TextUnit = 28.sp
+    val fontSize: Int = 18,
+    val lineHeight: Int = 24,
+    val readingMode: ReadingMode = ReadingMode.HORIZONTAL,
+    val brightness: Float = 100f,
+    val theme: ReaderTheme = ReaderTheme.Light
 )
