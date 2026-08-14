4adcf4dbaab263517b73c45d1f2b232f2d2380de
Andrey Filimonov <andrfil05@mail.ru>
2026-08-14 16:51:13 +0600

    fix: replace Result to MyResult

--- shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/domain/repository/BookRepository.kt
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/domain/repository/BookRepository.kt
@@ -1,5 +1,7 @@
 package com.filimonov.mylibrary.feature.library.domain.repository
 
+import com.filimonov.mylibrary.core.result.MyResult
+import com.filimonov.mylibrary.feature.library.domain.error.LibraryError
 import com.filimonov.mylibrary.feature.library.domain.model.Book
 import kotlinx.coroutines.flow.Flow
 
@@ -7,7 +9,7 @@ interface BookRepository {
 
     fun observeBooks(): Flow<List<Book>>
 
-    suspend fun addBook(bookPath: String): Result<Unit>
+    suspend fun addBook(bookPath: String): MyResult<Unit, LibraryError>
 
     suspend fun deleteBook(id: Long)
 
