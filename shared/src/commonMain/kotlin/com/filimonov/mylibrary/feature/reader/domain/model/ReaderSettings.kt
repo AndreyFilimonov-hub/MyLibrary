2cd9402ba22d9bc4b03fcde0b786ab527eb08c7c
Andrey Filimonov <andrfil05@mail.ru>
2026-08-19 13:44:06 +0600

    fix: change default settings

--- shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/model/ReaderSettings.kt
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/domain/model/ReaderSettings.kt
@@ -1,8 +1,8 @@
 package com.filimonov.mylibrary.feature.reader.domain.model
 
 data class ReaderSettings (
-    val fontSize: Int = 18,
-    val lineHeight: Int = 24,
+    val fontSize: Int = 14,
+    val lineHeight: Int = 21,
     val readingMode: ReadingMode = ReadingMode.HORIZONTAL,
     val brightness: Float = 100f,
     val theme: ReaderTheme = ReaderTheme.Light
