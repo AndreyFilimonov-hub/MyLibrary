0f6b4de790079dc638760ceec6e41fc05945cf1e
Andrey Filimonov <andrfil05@mail.ru>
2026-08-21 19:44:14 +0600

    feature(book-format-pdf): create PdfParser

---
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/library/data/parser/pdf/PdfParser.kt
@@ -0,0 +1,42 @@
+package com.filimonov.mylibrary.feature.library.data.parser.pdf
+
+import com.filimonov.mylibrary.core.storage.BookStorage
+import com.filimonov.mylibrary.feature.library.data.parser.BookFormat
+import com.filimonov.mylibrary.feature.library.domain.model.Book
+import okio.ByteString.Companion.toByteString
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+class PdfParser(
+    private val bookStorage: BookStorage,
+) {
+    @OptIn(ExperimentalUuidApi::class)
+    suspend fun parseBook(bytes: ByteArray): Book {
+        val metadata = extractPdfMetadata(bytes)
+
+        val title = metadata.title ?: "Unknown"
+        val author = metadata.author ?: "Unknown author"
+
+        val hash = bytes
+            .toByteString()
+            .sha256()
+            .hex()
+
+        val path = bookStorage.saveBook(
+            bytes = bytes,
+            bookFormat = BookFormat.PDF,
+            fileName = Uuid.random().toString()
+        )
+
+        return Book(
+            id = 0,
+            title = title,
+            author = author,
+            path = path,
+            coverPath = null,
+            hash = hash,
+            isFavorite = false,
+            isRead = false
+        )
+    }
+}
