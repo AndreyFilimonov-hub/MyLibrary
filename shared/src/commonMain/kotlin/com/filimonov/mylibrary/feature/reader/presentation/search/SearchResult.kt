f48b702c63f0c85c7d9e0b44aa2be855c75558b6
Andrey Filimonov <andrfil05@mail.ru>
2026-08-23 20:30:23 +0600

    feature(book-format-pdf): remove useless fields in SearchResult

--- shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/presentation/search/SearchResult.kt
+++ shared/src/commonMain/kotlin/com/filimonov/mylibrary/feature/reader/presentation/search/SearchResult.kt
@@ -1,8 +1,6 @@
 package com.filimonov.mylibrary.feature.reader.presentation.search
 
 data class SearchResult(
-    val chapterIndex: Int,
-    val pageIndexInChapter: Int,
     val globalPageIndex: Int,
     val snippet: String
 )
